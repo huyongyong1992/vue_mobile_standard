4cfc5fee1fbd218fc7708147a64765d1aabeee52
huyongyong1992 <young.hu@pdabc.com>
2019-04-24 17:05:03 +0800

    update

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -33,12 +33,12 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     compress: true,
     host: HOST || config.dev.host,
     port: PORT || config.dev.port,
-    open: config.dev.autoOpenBrowser,
+    open: true,
     overlay: config.dev.errorOverlay
       ? { warnings: false, errors: true }
       : false,
     publicPath: config.dev.assetsPublicPath,
-    proxy: config.dev.proxyTable,
+    proxy: config.dev.proxyTable || 'https://baidu.com',
     quiet: true, // necessary for FriendlyErrorsPlugin
     watchOptions: {
       poll: config.dev.poll,
